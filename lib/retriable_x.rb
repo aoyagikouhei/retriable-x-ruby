89620c2d77c5df1b375443af25e2eea72e8557c7
aoyagikouhei <aoyagi_kouhei@uniquevision.co.jp>
2024-04-28 17:25:04 +0900

    first

---
+++ lib/retriable_x.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "retriable_x/version"
+
+module RetriableX
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/retriable_x/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RetriableX
+  VERSION = "0.1.0"
+end
